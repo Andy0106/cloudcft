a4dfebccfd25a8d5fa65a6dcceffe42e8232bd9f
27Onion Nebell <57032603+onion108@users.noreply.github.com>
2022-10-06 20:23:30 +0800

    [chore] formatted code

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,25 +1,23 @@
 use std::collections::HashMap;
-pub enum token_type{
+pub enum token_type {
     EOF,
     NAME,
     DATA_TYPE,
     IGNORE,
     VARIABLE,
     STM_END,
-    EQUARE
+    EQUARE,
 }
-pub struct Token(pub token_type,pub usize,pub Option<String>);
+pub struct Token(pub token_type, pub usize, pub Option<String>);
 
-pub struct Lexer{
+pub struct Lexer {
     src: Vec<String>,
     line: usize,
     ptr: usize,
     pub next_token_info: token_type,
     next_token: Token,
-    words: HashMap<String,Token>
+    words: HashMap<String, Token>,
 }
-impl Lexer{
-    pub fn new(source: String){
-        
-    }
+impl Lexer {
+    pub fn new(source: String) {}
 }
