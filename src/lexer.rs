82c4a8fa675522dea96d06a5da0b6d2badc54798
Bdbmzwsc <chinabdbmzwsc@outlook.com>
2022-10-06 15:36:55 +0800

    added lexer base

---
+++ src/lexer.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+pub enum token_type{
+    EOF,
+    NAME,
+    DATA_TYPE,
+    IGNORE,
+    VARIABLE,
+    STM_END,
+    EQUARE
+}
+pub struct Token(pub token_type,pub usize,pub Option<String>);
+
+pub struct Lexer{
+    src: Vec<String>,
+    line: usize,
+    ptr: usize,
+    pub next_token_info: token_type,
+    next_token: Token,
+    words: HashMap<String,Token>
+}
+impl Lexer{
+    
+}
