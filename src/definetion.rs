50f53a4322931575b9dd3a270df8686f9cb8fe38
Bdbmzwsc <chinabdbmzwsc@outlook.com>
2022-10-07 16:47:36 +0800

    added definetion and construct for lexer

---
+++ src/definetion.rs
@@ -0,0 +1,11 @@
+use std::collections::HashMap;
+pub enum token_type {
+    EOF,
+    NAME,
+    DATA_TYPE,
+    IGNORE,
+    VARIABLE,
+    STM_END,
+    EQUAL,
+    NONE
+}
--- src/lexer.rs
+++ src/lexer.rs
@@ -1,23 +1,22 @@
 use std::collections::HashMap;
-pub enum token_type {
-    EOF,
-    NAME,
-    DATA_TYPE,
-    IGNORE,
-    VARIABLE,
-    STM_END,
-    EQUAL,
-}
+use super::definetion::token_type;
 pub struct Token(pub token_type, pub usize, pub Option<String>);
 
 pub struct Lexer {
-    src: Vec<String>,
+    src: String,
     line: usize,
     ptr: usize,
     pub next_token_info: token_type,
-    next_token: Token,
-    words: HashMap<String, Token>,
+    next_token: Token
 }
 impl Lexer {
-    pub fn new(source: String) {}
+    pub fn new(source: String) -> Self {
+        Lexer {
+            src: source,
+            line: 0,
+            ptr: 0,
+            next_token_info: token_type::NONE,
+            next_token: Token(token_type::NONE,0,None)
+        }
+    }
 }
