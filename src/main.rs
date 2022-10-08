271f2141c02221f4918c50e099397993067b62fd
Bdbmzwsc <chinabdbmzwsc@outlook.com>
2022-10-08 13:34:12 +0800

    [fix]: fix main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs;
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let content = fs::read_to_string(args[1].clone()).expect("Error about something");
+    /*  let args: Vec<String> = env::args().collect();
+    let content = fs::read_to_string(args[1].clone()).expect("Error about something");*/
 }
